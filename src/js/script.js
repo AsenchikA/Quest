780998027f23ca79d9088b55efb30764979f264a
Asenchik Anastasiya <n-asenchik@yandex.ru>
2017-05-02 20:16:05 +0400

    Верстка блока reviews

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 2,
+    margin: 8,
+    nav: true,
+    navClass: ['reviews__arrows reviews__arrows--prev','reviews__arrows reviews__arrows--next'],
+    navText: ['',''],
+  });
+});
