9866417471fff18b47d866cf64eebe231f59e9cb
Asenchik Anastasiya <n-asenchik@yandex.ru>
2017-05-03 21:44:21 +0400

    Добавлена навигация по странице

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+  //карусель в отзывах
+
   $(".owl-carousel").owlCarousel({
     loop: true,
     items: 2,
@@ -7,4 +10,18 @@ $(document).ready(function(){
     navClass: ['reviews__arrows reviews__arrows--prev','reviews__arrows reviews__arrows--next'],
     navText: ['',''],
   });
+
+  //плавная прокрутка страницы
+
+  function anchorScroll(boxAnchorLink){
+    $(boxAnchorLink + ' a').on('click', function(e){
+      e.preventDefault();
+      var attr = $(this).attr('href').substring(1);
+      var currentPosition = $(document).scrollTop();
+      var idPosition = $('#'+attr).offset().top;
+      var scrollTime = Math.abs(currentPosition - idPosition) / 3;
+      $('body,html').animate({'scrollTop':idPosition},scrollTime);
+    });
+  };
+anchorScroll('#main-menu');
 });
